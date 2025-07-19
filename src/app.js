fa2709cbb2849f9e6652f84bbfaa532559352b11
Felipe Santos <jfcardososantos@gmail.com>
2025-07-18 23:18:32 -0300

    cors9

--- src/app.js
+++ src/app.js
@@ -13,8 +13,19 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-
-app.use(cors());
+// CORS - CONFIGURAÇÃO SIMPLES E GARANTIDA
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 // Conexão com o banco
 export const pool = new Pool({
